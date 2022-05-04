8de33dd6d85003cbfc87df0733e67b0751beec5a
Wilson Brandão <wilsonbrandaocosta@gmail.com>
2022-05-04 10:42:12 -0300

    criacao pagina ver projeto

--- js/main.js
+++ js/main.js
@@ -8,7 +8,19 @@ const projetos = [
         textoDescricao: 'O Copan constitui um marco na paisagem da capital paulista. Mesmo quem nunca entrou no edifício identifica, de longe, as curvas que o compõem. Em meio à intensa verticalização do centro, os brises horizontais reforçam a presença do edifício no céu de São Paulo. No térreo, a galeria instalada no interior de um volume marrom conecta-se à rua por cinco acessos, praticamente as únicas aberturas desenhadas nesse embasamento maciço. As portarias de cada bloco de apartamento (seis, ao todo) são independentes entre si e surgem discretas ora próximo às entradas, ora entre a vitrine das lojas. A galeria acompanha a inclinação do chão da cidade. Da calçada, a continuidade do revestimento do piso e a ausência de degraus conduzem o usuário à rua interna que se forma. Como numa rua qualquer, é na soleira das lojas que o desnível será claramente marcado, a fim de garantir a concordância da laje horizontal plana com o piso inclinado, da cidade que entra no edifício. O relevo natural será dissolvido pouco a pouco nos pavimentos superiores da base, até que se atinja a laje plana horizontal dos trinta e dois pavimentos tipo em que se distribuem as 1.160 unidades habitacionais.',
         autorTexto: 'Carolina Silva Oukawa',
         linkArchDaily: 'https://www.archdaily.com.br/br/876920/classicos-da-arquitetura-edificio-copan-oscar-niemeyer?ad_source=search&ad_medium=projects_tab',
-        imagens: ['a','b','c']
+        imagens: ['escritorio-oscar-niemayer-edificio-copan/oscar-niemayer-edificio-copan-img-1.jpg',
+        'escritorio-oscar-niemayer-edificio-copan/oscar-niemayer-edificio-copan-img-2.jpg',
+        'escritorio-oscar-niemayer-edificio-copan/oscar-niemayer-edificio-copan-img-3.jpg',
+        'escritorio-oscar-niemayer-edificio-copan/oscar-niemayer-edificio-copan-img-4.jpg',
+        'escritorio-oscar-niemayer-edificio-copan/oscar-niemayer-edificio-copan-img-5.jpg',
+        'escritorio-oscar-niemayer-edificio-copan/oscar-niemayer-edificio-copan-img-6.jpg',
+        'escritorio-oscar-niemayer-edificio-copan/oscar-niemayer-edificio-copan-img-6.jpg',
+        'escritorio-oscar-niemayer-edificio-copan/oscar-niemayer-edificio-copan-img-7.jpg',
+        'escritorio-oscar-niemayer-edificio-copan/oscar-niemayer-edificio-copan-img-8.jpg',
+        'escritorio-oscar-niemayer-edificio-copan/oscar-niemayer-edificio-copan-img-9.jpg',
+        'escritorio-oscar-niemayer-edificio-copan/oscar-niemayer-edificio-copan-img-10.jpg',
+        'escritorio-oscar-niemayer-edificio-copan/oscar-niemayer-edificio-copan-img-11.jpg',
+        'escritorio-oscar-niemayer-edificio-copan/oscar-niemayer-edificio-copan-img-12.jpg',]
     }, 
     {
         titulo: 'Congresso Nacional',
@@ -16,10 +28,17 @@ const projetos = [
         ano: '1960',
         banner: 'escritorio-oscar-niemayer-congresso-nacional/oscar-niemayer-congresso-nacional-banner.jpg', 
         paragrafoBanner: 'O palácio do Congresso Nacional conta com uma localização privilegiada entre os edifícios públicos de Oscar Niemeyer em Brasília.',
-        textoDescricao: 'Localizado na cabeceira do plano piloto de Lúcio Costa, e como o único edifício sobre o canteiro central da asa leste do Eixo Monumental, o palácio do Congresso Nacional conta com uma localização privilegiada entre os edifícios públicos de Oscar Niemeyer em Brasília. O mais sóbrio dos palácios da Praça dos Três Poderes, o Congresso Nacional reflete a forte influência de Le Corbusier, ao mesmo tempo em que insinua as formas mais românticas e caprichosas que caracterizam o modernismo brasileiro de Niemeyer. O conceito de uma cidade-capital projetada no interior do país remonta à independência do Brasil de Portugal, após as Guerras Napoleônicas, e foi até mesmo consagrada na primeira Constituição Republicana do Brasil em 1891. Foi quando o amigo de Niemeyer, Juscelino Kubitschek, elegeu-se presidente em 1956 que a transformação começou a ocorrer. omeado diretor das obras de Brasília, "Niemeyer desfrutou de carta branca sobre a tomada de decisão artística na criação dos principais monumentos da cidade". Para acalmar a crítica de que Kubitschek havia descumprido uma lei que exigia concursos públicos para edifícios públicos, o projeto para o plano piloto de Brasília foi aberto a um concurso nacional. Niemeyer era membro do júri, e o projeto foi concedido ao seu mentor Lúcio Costa em 1957. Como parte do plano de Kubitschek dos "Cinquenta anos em cinco", a nova capital foi projetada e construída com velocidade aparentemente impossível, e inaugurada em 21 de abril de 1960. Não sendo, talvez, a mais famosa ou popular das estruturas monumentais de Brasília (o selo da cidade, por exemplo, remete aos arcos parabólicos invertidos na residência presidencial do Palácio da Alvorada ), o Congresso Nacional é certamente mais proeminente e simbólica de Brasília como sede do governo nacional. Aqui o plano urbano de Costa e a arquitetura de Niemeyer unem-se literalmente: as duas avenidas que marcam o Eixo Monumental erguem-se sobre taludes para combinar com o nível da cobertura do embasamento do edifício do Congresso Nacional e segmentos triangulares se estendem de cada canto da grande cobertura plana, em balanço, para apenas tocar as bordas das estradas. sElevando-se acima do telhado plano, duas cúpulas indicam as câmaras do legislativo brasileiro. Anteriormente alojados em dois edifícios separados no Rio de Janeiro, Niemeyer reuniu as duas câmaras legislativas em Brasília. Inspirado em outras estruturas do tipo, como o Capitólio dos EUA em Washington, DC, ou a Basílica de São Pedro em Roma, a cúpula sobre a câmara do Senado tem a forma de uma abóbada parabólica rasa. Em contraste, para a Câmara dos Deputados maior, Niemeyer inverteu a cúpula simbólica, criando a forma de uma tigela. Uma longa rampa leva da rodovia até o edifício. Dividido em dois segmentos, uma seção de rampa leva à entrada do edifício, enquanto a outra leva à cobertura do embasamento, revestida em mármore. Originalmente concebida como uma praça pública, a cobertura, desde então, tem permanecido fechada devido a preocupações de segurança.',
+        textoDescricao: '    Localizado na cabeceira do plano piloto de Lúcio Costa, e como o único edifício sobre o canteiro central da asa leste do Eixo Monumental, o palácio do Congresso Nacional conta com uma localização privilegiada entre os edifícios públicos de Oscar Niemeyer em Brasília. O mais sóbrio dos palácios da Praça dos Três Poderes, o Congresso Nacional reflete a forte influência de Le Corbusier, ao mesmo tempo em que insinua as formas mais românticas e caprichosas que caracterizam o modernismo brasileiro de Niemeyer. O conceito de uma cidade-capital projetada no interior do país remonta à independência do Brasil de Portugal, após as Guerras Napoleônicas, e foi até mesmo consagrada na primeira Constituição Republicana do Brasil em 1891. Foi quando o amigo de Niemeyer, Juscelino Kubitschek, elegeu-se presidente em 1956 que a transformação começou a ocorrer. omeado diretor das obras de Brasília, "Niemeyer desfrutou de carta branca sobre a tomada de decisão artística na criação dos principais monumentos da cidade". Para acalmar a crítica de que Kubitschek havia descumprido uma lei que exigia concursos públicos para edifícios públicos, o projeto para o plano piloto de Brasília foi aberto a um concurso nacional. Niemeyer era membro do júri, e o projeto foi concedido ao seu mentor Lúcio Costa em 1957. Como parte do plano de Kubitschek dos "Cinquenta anos em cinco", a nova capital foi projetada e construída com velocidade aparentemente impossível, e inaugurada em 21 de abril de 1960. Não sendo, talvez, a mais famosa ou popular das estruturas monumentais de Brasília (o selo da cidade, por exemplo, remete aos arcos parabólicos invertidos na residência presidencial do Palácio da Alvorada ), o Congresso Nacional é certamente mais proeminente e simbólica de Brasília como sede do governo nacional. Aqui o plano urbano de Costa e a arquitetura de Niemeyer unem-se literalmente: as duas avenidas que marcam o Eixo Monumental erguem-se sobre taludes para combinar com o nível da cobertura do embasamento do edifício do Congresso Nacional e segmentos triangulares se estendem de cada canto da grande cobertura plana, em balanço, para apenas tocar as bordas das estradas. Elevando-se acima do telhado plano, duas cúpulas indicam as câmaras do legislativo brasileiro. Anteriormente alojados em dois edifícios separados no Rio de Janeiro, Niemeyer reuniu as duas câmaras legislativas em Brasília. Inspirado em outras estruturas do tipo, como o Capitólio dos EUA em Washington, DC, ou a Basílica de São Pedro em Roma, a cúpula sobre a câmara do Senado tem a forma de uma abóbada parabólica rasa. Em contraste, para a Câmara dos Deputados maior, Niemeyer inverteu a cúpula simbólica, criando a forma de uma tigela. Uma longa rampa leva da rodovia até o edifício. Dividido em dois segmentos, uma seção de rampa leva à entrada do edifício, enquanto a outra leva à cobertura do embasamento, revestida em mármore. Originalmente concebida como uma praça pública, a cobertura, desde então, tem permanecido fechada devido a preocupações de segurança.',
         autorTexto: 'David Douglass-Jaimes',
         linkArchDaily: 'https://www.archdaily.com.br/br/803043/classicos-da-arquitetura-congresso-nacional-oscar-niemeyer?ad_source=search&ad_medium=projects_tab',
-        imagens: ['a','b','c']
+        imagens: ['escritorio-oscar-niemayer-congresso-nacional/oscar-niemayer-congresso-nacional-img-1.jpg',
+        'escritorio-oscar-niemayer-congresso-nacional/oscar-niemayer-congresso-nacional-img-2.jpg',
+        'escritorio-oscar-niemayer-congresso-nacional/oscar-niemayer-congresso-nacional-img-3.jpg',
+        'escritorio-oscar-niemayer-congresso-nacional/oscar-niemayer-congresso-nacional-img-4.jpg',
+        'escritorio-oscar-niemayer-congresso-nacional/oscar-niemayer-congresso-nacional-img-5.jpg',
+        'escritorio-oscar-niemayer-congresso-nacional/oscar-niemayer-congresso-nacional-img-6.jpg',
+        'escritorio-oscar-niemayer-congresso-nacional/oscar-niemayer-congresso-nacional-img-7.jpg',
+        'escritorio-oscar-niemayer-congresso-nacional/oscar-niemayer-congresso-nacional-img-8.jpg']
     }, 
     {
         titulo: 'Catedral de Brasília',
@@ -30,16 +49,53 @@ const projetos = [
         textoDescricao: 'A Catedral Metropolitana de Nossa Senhora Aparecida, popularmente conhecida como Catedral de Brasília, foi projetada por Oscar Niemeyer e construída entre 1959 e 1970, e faz parte do conjunto inicial de edifícios que compõem o Eixo Monumental da capital brasileira. O edifício é definido pelos seus dezesseis pilares de concreto em forma de bumerangue, que partem de uma planta circular de setenta metros de diâmetro, rodeada por um espelho dágua, e sobem inclinadamente até tocar uns aos outros. O cálculo estrutural desse e dos demais edifícios projetados por Oscar Niemeyer para o conjunto original de Brasília foi feito por Joaquim Cardozo. A catedral em sí está um nivel abaixo do plano de acesso; o edifício é, assim, meramente sua coberta. Seu acesso dá-se por um caminho criado por quatro esculturas, representando os evengelistas, que levam a uma rampa descendente, estreita e escura. O contraste entre o que foi visto externamente e o interior que se faz presente é supreendente. Os vitrais que fazem os fechamentos entre os pilares dotam a nave da catedral de abundante luz. A monumentalidade interna parece estar além da majestuosidade externa. Ao lado do edifício da Catedral está o campanário, cuja solução estrutural é novamente inusitada. Uma barra linear, que apoia sobre ela quatro sinos, sustentada por um único ponto central, que descem formando quatro pilares em suaves curvas. Do lado oposto está o batistério, finalizando a estrutura tripartida do conjunto. Assim como a Catedral, ele também é um espaço enterrado, mas, ao contrário daquela, sua coberta é uma casca ovóide opaca, levemente iluminada por aberturas laterais. A Catedral de Brasília é sua obra paradigmática, onde a concepção arquitetônica é reduzida ao mínimo, à sua estrutura, ao mesmo tempo que a concepção estrutural é levada ao máximo de suas possibilidades. Como disse o próprio arquiteto: “quando a estrutura está feita, o edifício está pronto”.',
         autorTexto: 'Igor Fracalossi',
         linkArchDaily: 'https://www.archdaily.com.br/br/01-14553/classicos-da-arquitetura-catedral-de-brasilia-oscar-niemeyer?ad_medium=office_landing&ad_name=article',
-        imagens: ['a','b','c']
+        imagens: [
+        'escritorio-oscar-niemayer-catedral-de-brasilia/oscar-niemayer-catedrau-brasilia-img-1.jpg',
+        'escritorio-oscar-niemayer-catedral-de-brasilia/oscar-niemayer-catedrau-brasilia-img-2.jpg',
+        'escritorio-oscar-niemayer-catedral-de-brasilia/oscar-niemayer-catedrau-brasilia-img-3.jpg',
+        'escritorio-oscar-niemayer-catedral-de-brasilia/oscar-niemayer-catedrau-brasilia-img-4.jpg',
+        'escritorio-oscar-niemayer-catedral-de-brasilia/oscar-niemayer-catedrau-brasilia-img-5.jpg',
+        'escritorio-oscar-niemayer-catedral-de-brasilia/oscar-niemayer-catedrau-brasilia-img-6.jpg',
+        'escritorio-oscar-niemayer-catedral-de-brasilia/oscar-niemayer-catedrau-brasilia-img-7.jpg',
+        'escritorio-oscar-niemayer-catedral-de-brasilia/oscar-niemayer-catedrau-brasilia-img-8.jpg']
+    }, 
+    {
+        titulo: 'Casa das Canoas',
+        escritorio: 'Oscar Niemeyer',
+        ano: '1954', 
+        banner: 'escritorio-oscar-niemayer-casa-das-canoas/escritorio-oscar-niemayer-casa-das-canoas-banner.jpg', 
+        paragrafoBanner: 'A Casa das Canoas, projetada pelo e para o próprio arquiteto Oscar Niemeyer entre 1950 e 1954, é sem sombra de dúvidas uma das obras mestras da arquitetura residencial moderna.',
+
+        textoDescricao: 'A Casa das Canoas, projetada pelo e para o próprio arquiteto Oscar Niemeyer entre 1950 e 1954, é sem sombra de dúvidas uma das obras mestras da arquitetura residencial moderna, ao lado das famosas Ville Savoye (1929) de Le Corbusier, Falling Water House (1935-37) de Frank Lloyd Wright, Glass House (1945-49) de Philip Johnson e Farnsworth House (1945-51) de Mies van der Rohe. Em Canoas, Niemeyer sintetiza seus ideais de forma já experimentados desde o Pavilhão Brasileiro para a Expo Nova Iorque de 1939 e no conjunto da Pampulha (1940-44) e que seriam implementados, pouco tempo depois, no Edifício Copan (1951-66) e no conjunto do Parque do Ibirapuera (1951-54). A coberta, uma sinuosa laje plana, é mais uma vez o que define o projeto. Porém, aqui, a natureza cria um rico contraste em harmonia com o construido. A casa insere -se e destaca-se na paisagem, ao mesmo tempo que a natureza é enfatizada por ela. Essa inserção é enfatizada pela presença de uma grande rocha incrustada à casa. Todo o projeto gira em torno e parece surgir dela. Para quem está dentro, essa rocha parece atravessar as paredes e janelas, formando parte íntegra do ambiente. Pela área externa, a rocha parece brotar do interior da casa e deslizar suavemente pela piscina. Mais que um condicionante do projeto, Niemeyer utiliza a natureza como um elemento configurador de projeto, assim como a laje, a piscina, os esbeltos pilares tubulares e as transparentes esquadrias de caixilhos negros. Niemeyer novamente faz uso do artifício de enterrar ambientes. Na Casa de Canoas, o nível de chegada, protegido pela sinuosa coberta, é formado pelos ambientes sociais -salas de estar e jantar-, cozinha e lavabo, em um espaço aberto, sombreado e transparente. Já a área íntima da casa situa-se completamente do nível inferior, acessado por uma escada que também surge lateralmente àquela rocha. No entanto, esse nivel, mais que enterrado, é mais uma estratégia projetual que dota o terreno descendente de caráter projetual. Os ambientes íntimos, assim como o nivel principal, também usufrui das visuais permitidas e, sobretudo, da luz natural, controlada através de singulares esquadrias e aberturas. A Casa das Canoas repousa, assim, sobre um amplo platô, imperceptível como tal, criando patios externos planos e protegidos por discretos gradis, sob o qual está os ambientes específicos de uma casa. Esta mesma solução -uma laje plana ortogonal que se faz de piso, sobre a qual surge uma coberta sinuosa e marcante-, será determinante, alguns anos depois, para o projeto do Congresso Nacional em Brasília.',
+
+        autorTexto: 'Igor Fracalossi',
+
+        linkArchDaily: 'https://www.archdaily.com.br/br/01-14512/classicos-da-arquitetura-casa-das-canoas-oscar-niemeyer?ad_source=search&ad_medium=projects_tab',
+
+        imagens: [
+        'escritorio-oscar-niemayer-casa-das-canoas/escritorio-oscar-niemayer-casa-das-canoas-img-1.jpg',
+        'escritorio-oscar-niemayer-casa-das-canoas/escritorio-oscar-niemayer-casa-das-canoas-img-2.jpg',
+        'escritorio-oscar-niemayer-casa-das-canoas/escritorio-oscar-niemayer-casa-das-canoas-img-3.jpg',
+        'escritorio-oscar-niemayer-casa-das-canoas/escritorio-oscar-niemayer-casa-das-canoas-img-4.jpg',
+        'escritorio-oscar-niemayer-casa-das-canoas/escritorio-oscar-niemayer-casa-das-canoas-img-5.jpg',
+        'escritorio-oscar-niemayer-casa-das-canoas/escritorio-oscar-niemayer-casa-das-canoas-img-6.jpg',
+        'escritorio-oscar-niemayer-casa-das-canoas/escritorio-oscar-niemayer-casa-das-canoas-img-7.jpg',
+        'escritorio-oscar-niemayer-casa-das-canoas/escritorio-oscar-niemayer-casa-das-canoas-img-8.jpg',
+        'escritorio-oscar-niemayer-casa-das-canoas/escritorio-oscar-niemayer-casa-das-canoas-img-9.jpg',
+      ]
     }, {}
 ]
 
 let randomBanner = "";
 
 const buttonVerProjeto = document.querySelector("#button-ver-projeto");
+const buttonVerProjetoModal = document.querySelector("#button-ver-projeto-modal");
 homePadrao();
 
-buttonVerProjeto.addEventListener("click", () => {
+buttonVerProjeto.addEventListener("click", VerProjeto);
+buttonVerProjetoModal.addEventListener("click", VerProjeto);
+
+function VerProjeto () {
     const carrosel = document.querySelectorAll(".carrosel"); 
     const button = document.querySelectorAll(".button");
     const paragrafoBanner = document.querySelector("#paragrafo-banner");
@@ -52,7 +108,8 @@ buttonVerProjeto.addEventListener("click", () => {
     button.forEach((item) => {
         item.style.display = "none"; //ou inline-block
     })
-})
+    buttonVerProjetoModal.addEventListener("click", desativaModal);
+}
 
 
 function homePadrao() {
@@ -92,8 +149,6 @@ function GeraBanner(projetos) {
 }
 
 
-
-
 function GerarModal(random, projetos) {
     const imgBannerModal = document.querySelector("#modal-banner");
     const tituloModal = document.querySelector("#titulo-modal");
@@ -113,25 +168,28 @@ function CriarVerProjeto(projetos) {
     const conteudoProjeto = document.querySelector(".conteudo-ver-projeto");
     conteudoProjeto.classList.add("mostrar");
 
-    conteudoProjeto.appendChild(criarParagrafo(projetos[randomBanner].textoDescricao))
+    conteudoProjeto.appendChild(criarParagrafo(projetos[randomBanner].textoDescricao));
+    conteudoProjeto.appendChild(criarParagrafo(projetos[randomBanner].autorTexto));
 
     projetos[randomBanner].imagens.forEach((imagemSrc) => {
-        let elementoImg = criarImgProjeto(imagemSrc, "img-projeto");
+        let imgSrc = "../img/projects/" + imagemSrc;
+        let elementoImg = criarImgProjeto(imgSrc, "img-projeto");
         conteudoProjeto.appendChild(elementoImg);
     });
+    
 }
 
 
 function criarImgProjeto(dado, classe){
     let img = document.createElement("img");
     img.classList.add(classe);
-    img.setAttribute("src", dado)
+    img.setAttribute("src", dado);
     return img; 
 }
 
 function criarParagrafo(texto){
     let p = document.createElement("p");
     p.setAttribute("id", "paragrafo-ver-projeto");
-    p.textContent = texto;
+    p.textContent = "Escrito por: " + texto;
     return p; 
 }
--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,18 @@
 function ModalMaisInfo(modalID) {
     const modal = document.getElementById(modalID);
-    const body = document.querySelector("#scroll");
     modal.classList.add("mostrar-modal");
     modal.classList.add("scroll");
-    modal.addEventListener('click', (event) => {
-        if(event.target.id == "modal-mais-informacoes" || event.target.id == "button-fechar-modal"){
-            modal.classList.remove("mostrar-modal")
-            body.classList.remove("scroll")
-        }
-    });
+    modal.addEventListener('click', desativaModal);
+}
+
+function desativaModal (event){
+    const modal = document.getElementById("modal-mais-informacoes");
+    const body = document.querySelector("#scroll");
+    if(event.target.id == "modal-mais-informacoes" || event.target.id == "button-fechar-modal"
+     || event.target.id == "button-ver-projeto-modal"){
+        modal.classList.remove("mostrar-modal")
+        body.classList.remove("scroll")
+    }
 }
 
 const botaoMaisInfo = document.querySelector('#button-mais-info');
